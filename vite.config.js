c00e2b5c46f17b57071482a16c67e56f8d0a7065
DIEGO NUNES <diegonunesabc@gmail.com>
2023-05-25 01:41:18 -0300

    Update vite.config.js
    commit 4

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/http://localhost:5173/Meu-Portifolio/",
+  base: '/Meu-Portifolio/',
   plugins: [react()],
 })
